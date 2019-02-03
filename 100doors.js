8887ad75dbbbcc91565ff24f35bda6e947ee75e9
eamobi <amobisomto@gmail.com>
2019-02-03 13:26:37 -0600

    solved 100doors problem

--- 100doors.js
+++ 100doors.js
@@ -1,5 +1,32 @@
 /* There are 100 doors in a row that are all initially closed. You make 100 passes by the doors. The first time through, visit every door and 'toggle' the door (if the door is closed, open it; if it is open, close it). The second time, only visit every 2nd door (i.e., door #2, #4, #6, ...) and toggle it. The third time, visit every 3rd door (i.e., door #3, #6, #9, ...), etc., until you only visit the 100th door.
  Implement a function to determine the state of the doors after the last pass. Return the final result in an array, with only the door number included in the array if it is open. */
 function getFinalOpenedDoors(numDoors){
-    
+    let doorAr = [];
+    //Set all the doors to be intitially closed 
+    for (let i = 1; i <= numDoors; i++){
+        doorAr[i] = 'closed';
+    }
+    //Go through the doors and toggle them.
+    for (let i = 1; i <= numDoors; i++){
+        for (let j = i; j <= numDoors; j+=i){
+            toggleDoor(doorAr,j);
+        }
+    }
+    //Put the opened doors in the array
+    let arrNums = [];
+    for (let i = 1; i <= numDoors; i++){
+        if (doorAr[i] === 'opened'){
+            arrNums.push(i);
+        }
+    }
+    return arrNums;
+
+}
+function toggleDoor(arr,index){
+    if (arr[index] === 'closed' ){
+        arr[index] = 'opened';
+    }
+    else if (arr[index] === 'opened'){
+        arr[index] = 'closed';
+    }
 }
